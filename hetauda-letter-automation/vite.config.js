2c106e6e3a1571678254b610fbad3f662b67c446
Paradox401 <sandeshsapkota371@gmail.com>
2025-04-11 09:43:26 +0545

    fix4

--- hetauda-letter-automation/vite.config.js
+++ hetauda-letter-automation/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'HetaudaLetterAutomation', // 👈 Replace with your repo name
+  base: '/HetaudaLetterAutomation/', // 👈 Replace with your repo name
 })
 
